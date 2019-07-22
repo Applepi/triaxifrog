dafb26782db8ff82f6e1ba322ca5618de9d5f6b3
Joseph Toney <josephtoney@my.unt.edu>
2019-07-22 09:48:29 -0500

    more work getting commands working

--- hdf5/testdata.py
+++ hdf5/testdata.py
@@ -13,28 +13,38 @@ elif len(sys.argv) == 3:
 else:
     print("Please provide 2D spectrum data and 1d position data as aruments")
 
-testdata = np.loadtxt(filename1, delimiter="\t")
-testdata = testdata.astype(int)
-posdata = np.loadtxt(filename2, delimiter="\t")
-
-with h5py.File('ifrog.hdf5', 'w') as f:
-    dset = f.create_dataset(filename1, data=testdata)
-    dset = f.create_dataset(filename2, data=posdata)
-
-hf = h5py.File('ifrog.hdf5', 'r')
-    
-with h5py.File('ifrog.hdf5', 'r') as f:
-   data = f[filename1]
-   stepdata = f[filename2]
 
 
 if len(sys.argv) == 4:
-    print(command)
+    print(command + ' starting')
     if command in ['plot']:
+        
         plt.matshow(testdata, aspect='auto', vmin=1000, vmax=16000)
         plt.show()
-    
+    if command in ['poscoro']:
+        for i in stepdata:
+            print(i)
+        hf.close()
+    if command in ['import']:
+        testdata = np.loadtxt(filename1, delimiter="\t")
+        testdata = testdata.astype(int)
+        posdata = np.loadtxt(filename2, delimiter="\t")
+        with h5py.File('ifrog.hdf5', 'w') as f:
+            dset = f.create_dataset(filename1, data=testdata)
+            dset = f.create_dataset(filename2, data=posdata)
+        print(command + ' complete')
 
+
+
+
+#hfr = h5py.File('ifrog.hdf5', 'r')
+#hfw = h5py.File('ifrog.hdf5', 'w')
+    
+#with h5py.File('ifrog.hdf5', 'r') as f:
+#   data = f[filename1]
+#   stepdata = f[filename2]
+
+        
 #stepdata = hf.get('20190717_880_bbo2_pos')
 
 #hf = h5py.File('test.hdf5', 'r')
@@ -46,7 +56,5 @@ if len(sys.argv) == 4:
 #    value = stepdata[i]
 #    newvalue = stepdata[i] * 10e-6 * 2 / 3 * 10e8
 #    stepdata[i] = newvalue
-
-
-
+    
 ##stepvalue(um)*10^-6*2/3*10^8
