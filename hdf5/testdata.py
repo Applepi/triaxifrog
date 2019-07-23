64ce0fc9d8cdb4a2919e0734d7c829f25e2dd9bc
Joseph Toney <josephtoney@my.unt.edu>
2019-07-23 10:44:43 -0500

    Clean up

--- hdf5/testdata.py
+++ hdf5/testdata.py
@@ -29,20 +29,15 @@ if len(sys.argv) == 4:
         f = h5py.File('ifrog.hdf5', 'r')
         corodata = f[filename2+'coro'][::]
         corowl = f['corowl']
-        print(corodata[:])
         coroinds = corodata[::].argsort()
-        print(coroinds[:-1])
         sorted_coro = corodata[coroinds[:-1]]
         sorted_grid = f[filename1][::,::][:,coroinds[:-1]]
-        print(np.shape(f[filename1][::]))
-        print(np.shape(corodata[::]))
-        print(np.shape(corowl[::]))
-        #print(f[filename1])
+        
+
         ft = pypret.FourierTransform(256, dt=2.5e-15)
         pulse = pypret.Pulse(ft, 400e-9)
         pulse.spectrum = pypret.lib.gaussian(pulse.wl, x0=800e-9, sigma=20e-9)
         # print the accurate FWHM of the temporal intensity envelope
-        print(pulse.fwhm(dt=pulse.dt/100))
         # propagate it through 1cm of BK7 (remove first ord)
         phase = np.exp(1.0j * pypret.material.BK7.k(pulse.wl) * 0.01)
         pulse.spectrum = pulse.spectrum * phase
@@ -52,58 +47,51 @@ if len(sys.argv) == 4:
         pnps.calculate(pulse.spectrum, sorted_coro)
         original_spectrum = pulse.spectrum
         # and plot it
-
         #mesh = pypret.mesh_data.MeshData(f[filename1][::],  corowl[::], corodata[::])
+        
         mesh = pypret.mesh_data.MeshData(sorted_grid[:,10000:],  corowl[::], sorted_coro[10000:])
         pypret.graphics.MeshDataPlot(mesh, plot=True)
-#        ret = pypret.retrieval.retriever.BaseRetriever._retrieve_begin(mesh, pulse.specturm ,weights=None)
+        #ret = pypret.retrieval.retriever.BaseRetriever._retrieve_begin(mesh, pulse.specturm ,weights=None)
+        
         ret = pypret.Retriever(pnps, "copra", verbose=True, maxiter=300)
         ret.retrieve(mesh, pulse.spectrum, weights=None)
         pypret.random_gaussian(pulse, 50e-15, phase_max=0.0)
-        print(mesh)
         pypret.graphics.MeshDataPlot(mesh, plot=True)
+
         #pypret.mesh_data.MeshData(f[filename1][::], corodata[::]) 
         f.close()
     if command in ['poscoro']:
-        #print(len(f[filename2]))
         index = -1 
         f = h5py.File('ifrog.hdf5', 'r+')
         corodata = f[filename2+'coro']
-        print(f[filename1])
         corowl = f['corowl']
         for i in f[filename2]:
-            #print(i)
             index = index + 1 
-            #print(index)
             #Magic number to set data around t=0
             magicnum = 9858.6325
             i = (i + magicnum) * (10e-6) * (2/(3*10e8))
             corodata[index] = i
             ##stepvalue(um)*10^-6*2/3*10^8
             #-9858.6325 = t=0
-            #print(i)
         index = -1
         for i in f[filename1]:
             index = index + 1
-            #print(index)
             wl = -0.81967 * index + 440
             corowl[index] = wl +100
-            #print(wl)
             #pix num to WL
             #y=-0.081967x+440
-           # #x = pixnum
+            #x = pixnum
             #y = wl (nm)
-        print(corowl[0])
         f.close()
     if command in ['import']:
         testdata = np.loadtxt(filename1, delimiter="\t")
         testdata = testdata.astype(int)
         posdata = np.loadtxt(filename2, delimiter="\t")
         with h5py.File('ifrog.hdf5', 'w') as f:
-            dset = f.create_dataset(filename1, data=testdata[:,:-1])
-            dset = f.create_dataset(filename2, data=posdata)
-            dset = f.create_dataset(filename2+'coro', data=posdata)
-            dset = f.create_dataset('corowl', data=np.zeros((1021)))
+            f.create_dataset(filename1, data=testdata[:,:-1])
+            f.create_dataset(filename2, data=posdata)
+            f.create_dataset(filename2+'coro', data=posdata)
+            f.create_dataset('corowl', data=np.zeros((1021)))
         print(command + ' complete')
 
 
